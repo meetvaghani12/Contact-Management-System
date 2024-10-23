3996a8c6eedb9b174026c9226e876ee9003e32d9
Meet Vaghani <meetvaghani1238@gmail.com>
2024-10-23 23:01:04 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://backend-contact-management-system.onrender.com/";
+const API_URL = "https://backend-contact-management-system.onrender.com/api";
 alert(API_URL)
 
 // Basic CRUD Operations
